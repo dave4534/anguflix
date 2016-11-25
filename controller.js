09d6615c71b6cb9f9b7876b6fcfc921894bca43a
uDave <uDave@Dave.local>
2016-11-25 13:54:16 +0200

    finished

--- controller.js
+++ controller.js
@@ -1,14 +1,34 @@
 app.controller('anguControl', function($scope, anguService){
   $scope.movieArray = anguService.movieArray;
   $scope.movieCart = anguService.movieCart;
+  $scope.budget = anguService.budget;
 
-  $scope.addToCart = function(index){
-  // function that adds selected movie to movieCart array
-    anguService.movieCart.push(anguService.movieArray[index]);
-    console.log($scope);
+  $scope.years = [];
+    for(var i = 1982; i <= 2016; i++){
+      $scope.years.push(i);
+    }
+
+
+
+  $scope.addToCart = function(index, movie){
+    removeFromBudget(movie.price, index);
+    $scope.movieCart[index] = movie;
+  };
+
+  var removeFromBudget = function(price, index){
+    // if movieCart doesn't include movie
+    if (!angular.isObject($scope.movieCart[index])){
+      $scope.budget -= price;
+    }
+    
   };
 
   $scope.arraySearch = '';
   $scope.movieSearch = '';
+
+  $scope.removeMovie = function($index, movie){
+    $scope.movieCart.splice($index, 1);
+    $scope.budget += movie.price;
+  };
 });
 
--- service.js
+++ service.js
@@ -1,25 +1,32 @@
 app.factory('anguService', function(){
+  var budget = 9;
   var movieArray = [
     {
       title: "Tarzan",
       year: "1999",
       description: "The movie is about the life of Tarzan. Tarzan was a small orphan who was raised by an ape named Kala since he was a child. He believed that this was his family, but on an expedition Jane Porter is rescued by Tarzan.",
       image: "http://static.comicvine.com/uploads/original/10/104544/4068923-tarzan-wallpaper-walt-disneys-tarzan-6248938-1024-768.jpg",
-      remove: false
+      remove: false,
+      id: 0,
+      price: 3
     },
     {
       title: "Lion King",
       year: "1994",
       description: "A young lion Prince is cast out of his pride by his cruel uncle, who claims he killed his father. While the uncle rules with an iron paw, the prince grows up beyond the Savannah, living by a philosophy: No worries for the rest of your days.",
       image: "http://cdn.collider.com/wp-content/uploads/2016/04/the-lion-king-image.jpg",
-      remove: false
+      remove: false,
+      id: 2,
+      price: 3
     },
     {
       title: "Beauty and the Beast",
       year: "1991",
       description: "A kickass woman named Belle who does not succumb to social norms gets crap from a bunch of village idiots, chief amongst them a total tool named Gaston. Belle shows everyone how great she is when she turns a beast (not Gaston) into a man. Love.",
       image: "http://img.lum.dolimg.com/v1/images/characters_beautyandthebeast_belle_852af5fe.jpeg?region=0,0,1536,788&width=1200",
-      remove: false
+      remove: false,
+      id: 1,
+      price: 3
     }
   ];
  
@@ -27,7 +34,8 @@ app.factory('anguService', function(){
 
   return {
     movieArray: movieArray,
-    movieCart: movieCart
+    movieCart: movieCart,
+    budget: budget
   };
 
 });
